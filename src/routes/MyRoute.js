891b0b78bd900f9d86e1c5585ab5e49d1e993a7e
Luciano Machado <lucianoomachados@gmail.com>
2024-10-28 19:37:17 -0300

    MyRoute - Rota personalizada e Privada

---
+++ src/routes/MyRoute.js
@@ -0,0 +1,32 @@
+import React from "react";
+import { Route, Redirect } from "react-router-dom";
+import PropTypes from "prop-types";
+
+export default function MyRoutes({ component: Component, isClosed, ...rest }) {
+  const isLoggedId = true;
+
+/*
+Essa funcão irá redirecionar o usuario para a rota /login
+caso o usuario esteja tentando acessar uma rota que precise de autenticacao,
+e depois ele retorna o usuario para a rota que ele estava antes de ser redirecionado.
+*/
+  if (isClosed && !isLoggedId) {
+    return (
+    <Redirect
+      to={{pathname: "/login", state:{ prevPath: rest.location.pathname} }}
+    />);
+  }
+
+  return <Route {...rest} component={Component} />;
+}
+
+// Definindo as propriedades padrão por isso isClosed não é obrigatorio.
+MyRoutes.defaultProps = {
+  isClosed: false,
+};
+
+MyRoutes.propTypes = {
+  component: PropTypes.oneOfType([PropTypes.element, PropTypes.func])//ele pode ser um elemento ou uma função
+    .isRequired,
+  isClosed: PropTypes.bool,
+};
