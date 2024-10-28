df8ac7a50268482135c0457e7f4431da7e30141a
Luciano Machado <lucianoomachados@gmail.com>
2024-10-28 17:26:52 -0300

    Primeiro commit

---
+++ src/pages/Login/index.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+import { Container } from "../../styles/GlobalStyles";
+import { Title, Paragrafo } from "./styled";
+
+export default function Login() {
+  return (
+    <Container>
+      <Title isRed={true}>
+      <h1>Login
+        <small>paeg</small>
+      </h1>
+      </Title>
+      <Paragrafo>Lorem ipsum dolor sit amet</ Paragrafo>
+      <button type="button">Enviar</button>
+    </ Container>
+  );
+}
---
+++ src/pages/Page404/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+import { Container } from "../../styles/GlobalStyles";
+
+
+export default function Page404() {
+  return (
+    <Container>
+      <h1>Essa página não existe</h1>
+    </Container>
+  );
+}
